6c2142644f0d22e0980afc791406e53fcee58bf8
thiagoteixas <thiagoteixas@gmail.com>
2021-06-28 15:34:55 -0300

    Dia 8.4

---
+++ trybe-exercises/exercises/Módulo 1 - Fundamentos do Desenvolvimento Web/8.4/sumEven/sumEvenReduce&Filter.js
@@ -0,0 +1,8 @@
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (number) => number % 2 === 0;
+const sumPair = (currentValue, number) => currentValue + number;
+
+const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+
+console.log(sumNumbers(numbers)); // 152
